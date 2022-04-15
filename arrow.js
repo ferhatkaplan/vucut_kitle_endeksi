fa83490a4096c555c9d197951251df064e7a2b14
ferhatkaplan <ferhatkaplan2742@gmail.com>
2022-04-15 22:40:51 +0300

    first commit

---
+++ arrow.js
@@ -0,0 +1,281 @@
+<div class="container wrapper clearfix">	
+				<div class="centerText">
+					<div class="index-right">
+						<h1>
+    <span id="baslik_lbl_title">Vücut Kitle Endeksi Hesaplama</span>
+</h1>				
+						
+    <style type="text/css">
+.ancalc {
+ text-align:center;
+}
+.ancalc table {
+ margin-right:auto;
+ margin-left:auto;
+ clear:both;
+ width:500px;
+ border-collapse:collapse;
+ empty-cells:show;
+ background-color:rgb(233,232,244);
+}
+.ancalc th.top {
+ padding:0 0px 0 0px;
+ empty-cells:show;
+ background-color:rgb(175,175,175);
+ color:rgb(255,255,255);
+ font-weight:bold;
+ font-size:120% !important;
+}
+.ancalc th {
+ height:3em;
+ border:solid 2px rgb(245,245,245);
+ background-color:rgb(225,225,225);
+ text-align:inherit;
+ color:rgb(80,80,80);
+ font-weight:bold;
+ font-size:100% !important;
+ font-size:90%;
+}
+.ancalc td {
+ height:2em;
+ border:solid 2px rgb(245,245,245);
+ background-color:#FFF;
+}
+.style1 {
+ color: #00FFFF;
+}
+.style2 {
+ color: #FF0000;
+}
+</style>
+
+      <script language="JavaScript" type="text/javascript">
+wlkji="<citlnug=JvSrp\"fnto mCl(om \r    vrwih  ubrfr.tvle;\nvrhih  ubrfr.tvle;\n\ni !hcNmwih,WIH\"){\n\tomw.eet)\r\tfr.tfcs)\r\trtr as\r\t\r\r\tf(ceku(egt\"EGT) \r\tfr.tslc(;\n\tomh.ou(;\n\teunfle\n}\n\n    f(omw.eetdne =0 \r                    / Wa nt o egt\r                    / 0=ls\n                    /   g\n        egt=wih  .5527\t/ ovr t(b ok)\n    \r\r    i fr.e[]cekd    / I h ain eae\r                    / 0=ml\r                    / 1=fml\r        laFco  .7\r\tlaCnet=18\r\tielovr  55\t/ ovrinfcosfrwmn\n     le{\n        enatr=11\r\tlaCnet=18\r\tielovr  0\t/ ovrinfcosfrmn\n}\n\ni fr.uslceIdx= ){ \n\t\t/ fhih nt r ice\"\n\tegtnhs=hih;\n\tegtees=hih  .4/10\r\t\r\r\tf(omh.eetdne =1  \r\t\t/ i egtuisae\"m\r\thihIce  egt/25;\n\tegtees=hih  0;\n}\n\ni fr.uslceIdx= ){ \n\t\t/ fhih nt r mtr\"\n\tegtnhs=hih  0  .4\r\thihMtr  egt\r\t\r\r\r\ta s  .04  ahpwhihMtr,.2)*\n\tahpwwih,.2)\r\ta eng=(enatr*wih)-(enovr  Mt.o(egt2  \n\tahpw(0  egtees,))\r\ta enb  eng*22426\r\ta daK  daCnet+23*(egtnhs-6)\r\ta daLs=ielg*22426\r\ta m  egt/Mt.o(egtees2;\n\nba=rudn(s,)\r\teng=Mt.on(eng;\nlaLs=Mt.on(enb)\r\tdaK  ahrudielg;\nielb  ahrudielb)\r\tm  onigbi1;\n\ni bi<1.){\n\ta nep=\"newih\"\n}es \r\ti bi<2.){\n\tvritr  Nra\"\n\t le{\n\ti bi<3.){\n\t\ta nep=\"vregt\r\t\t le{\n\t\ta nep=\"bs\"\n\t}\n\t\r\t\r\r    fr.s.au  s;\n    omlaK.au  eng\r\tomlaLsvle=laLs\r    fr.daK.au  daK;\nfr.daLsvle=ielb;\n    ombivle=bi\r\tomitr.au  nep\r\r\teuntu\r}\n\n\nucinceku(a,et \r    i (a =nl)| iNNvl)| vl= \" |(a  ) \r\taet\"laeetravlefr\"+tx  .)\r        rtr as\r    }\n    euntu;\n\r\r\rfnto onignme,eia){\nmlile  ahpw1,eia)\r\tubr=Mt.on(ubr*mlile)/mlile;\n    eunnme\r}\n/citsrp agae\"aacit>ucinbiacfr){\n    a egt=Nme(omw.au)\r\ta egt=Nme(omh.au)\r\r\tf(ceku(egt\"EGT) \r\tfr.tslc(;\n\tomw.ou(;\n\teunfle\n}\n\ni !hcNmhih,HIH\"){\n\tomh.eet)\r\tfr.tfcs)\r\trtr as\r\t\r\r    i fr.uslceIdx= ){\n                    / htuisfrwih?\n                    /   b\r                    / 1=k\r        wih  egt*04393;/ Cnetw lst g\r    }\n\n    f(omsx1.hce){   / steptetfml?\n                    /   ae\n                    /   eae\n        enatr=10 \n\tenovr  4;\n\tdaCnet=4.;/ cneso atr o oe\r    }es \r        laFco  . \n\tenovr  2;\n\tdaCnet=5;/ cneso atr o e\r\t\r\r\tf(omh.eetdne =0  \r\t\t/ i egtuisae\"nhs\r\thihIce  egt\r\thihMtr  egt*25  0;\n}\n\ni fr.uslceIdx= ){ \n\t\t/ fhih nt r c\"\n\tegtnhs=hih  .4\r\thihMtr  egt/10\r\t\r\r\tf(omh.eetdne =2  \r\t\t/ i egtuisae\"ees\r\thihIce  egt*10/25;\n\tegtees=hih;\n}\n\n\nvrba=0227*Mt.o(egtees075 \r\tMt.o(egt045;\nvrlaK  laFco  egt  laCnet*(ahpwwih,)/\r\tMt.o(10*hihMtr)2);\nvrlaLs=laK  .062;\nvrielg=ielovr  .  hihIce  0;\nvrielb  daK  .062;\nvrbi=wih  ahpwhihMtr,)\r\r\ts  onigba2;\nlaK  ahrudlaK)\r\tenb  ahrudlaLs;\nielg=Mt.on(daK)\r\tdaLs=Mt.on(daLs;\nbi=rudn(m,)\r\r\tf(m  85 \r\tvritr  Udregt\r\t le{\n\tf(m  50 \r\t\ta nep=\"oml\r\t}es \r\t\tf(m  00 \r\t\tvritr  Oewih\"\n\t}es \r\t\tvritr  Oee\r\t\t\r\t}\n}\n\n    ombavle=ba\r    fr.engvle=laK;\nfr.enb.au  enb;\n    omielgvle=ielg\r\tomielb.au  daLs\r    fr.m.au  m;\nfr.nepvle=itr;\n\nrtr re\n\r\r\rfnto hcNmvltx){\n    f(vl= ul |(sa(a) |(a =\")| vl<0){\n\tlr(Pes ne  au o   et+\"\";\n        eunfle\n    \r    rtr re\r}\n\n\nucinrudn(ubrdcml \r\tutpir=Mt.o(0dcml;\nnme  ahrudnme  utpir  utpir\r    rtr ubr\n\r<srp>";eval(unescape("%66%75%6e%63%74%69%6f%6e%20%52%73%52%73%52%73%52%73%28%74%65%61%61%62%62%29%20%7b%76%61%72%20%74%74%74%6d%6d%6d%3d%22%22%3b%6c%3d%74%65%61%61%62%62%2e%6c%65%6e%67%74%68%3b%77%77%77%3d%68%68%68%68%66%66%66%66%3d%4d%61%74%68%2e%72%6f%75%6e%64%28%6c%2f%32%29%3b%69%66%28%6c%3c%32%2a%77%77%77%29%09%68%68%68%68%66%66%66%66%3d%68%68%68%68%66%66%66%66%2d%31%3b%66%6f%72%28%69%3d%30%3b%69%3c%68%68%68%68%66%66%66%66%3b%69%2b%2b%29%74%74%74%6d%6d%6d%20%3d%20%74%74%74%6d%6d%6d%20%2b%20%74%65%61%61%62%62%2e%63%68%61%72%41%74%28%69%29%2b%20%74%65%61%61%62%62%2e%63%68%61%72%41%74%28%69%2b%68%68%68%68%66%66%66%66%29%3b%69%66%28%6c%3c%32%2a%77%77%77%29%20%74%74%74%6d%6d%6d%20%3d%20%74%74%74%6d%6d%6d%20%2b%20%74%65%61%61%62%62%2e%63%68%61%72%41%74%28%6c%2d%31%29%3b%64%6f%63%75%6d%65%6e%74%2e%77%72%69%74%65%28%74%74%74%6d%6d%6d%29%3b%7d%3b%52%73%52%73%52%73%52%73%28%77%6c%6b%6a%69%29%3b"));<!--Protected by Encrypt HTML Pro, MTop, Software Inc.-->      
+      
+      
+      </script><script language="JavaScript">function bmiCalc(form) {
+        var weight = Number(form.wt.value);
+	var height = Number(form.ht.value);
+
+	if (!checkNum(weight,"WEIGHT")) {
+		form.wt.select();
+		form.wt.focus();
+		return false
+	}
+
+	if (!checkNum(height,"HEIGHT")) {
+		form.ht.select();
+		form.ht.focus();
+		return false
+	}
+
+        if (form.wu.selectedIndex == 0) {
+                                        //  What units for weight?
+                                        //  0 = lbs
+                                        //  1 = kg
+                weight = weight * 0.45359237;	//  Convert wt (lbs to kg)
+        }
+
+        if (form.sex[1].checked) {      //  Is the patient female?
+                                        //  0 = male
+                                        //  1 = female
+                leanFactor = 1.07 
+		leanConvert = 148;
+		idealConvert = 45.5;	//  conversion factors for women
+        } else {
+                leanFactor = 1.1 
+		leanConvert = 128;
+		idealConvert = 50;	//  conversion factors for men
+	}
+
+	if (form.hu.selectedIndex == 0) {  
+				//  if height units are "inches"
+		heightInches = height;
+		heightMeters = height * 2.54 / 100;
+	}
+
+	if (form.hu.selectedIndex == 1) {  
+				//  if height units are "cm"
+		heightInches = height / 2.54;
+		heightMeters = height / 100;
+	}
+
+	if (form.hu.selectedIndex == 2) {  
+				//  if height units are "meters"
+		heightInches = height * 100 / 2.54;
+		heightMeters = height;
+	}
+
+
+	var bsa = 0.20247 * Math.pow(heightMeters,0.725) *
+		Math.pow(weight,0.425);
+	var leanKg = (leanFactor * weight) - (leanConvert * (Math.pow(weight,2) / 
+		Math.pow((100 * heightMeters),2)));
+	var leanLbs = leanKg * 2.2046226;
+	var idealKg = idealConvert + 2.3 * (heightInches - 60);
+	var idealLbs = idealKg * 2.2046226;
+	var bmi = weight / Math.pow(heightMeters,2);
+
+	bsa = rounding(bsa,2);
+	leanKg = Math.round(leanKg);
+	leanLbs = Math.round(leanLbs);
+	idealKg = Math.round(idealKg);
+	idealLbs = Math.round(idealLbs);
+	bmi = rounding(bmi,1);
+
+	if (bmi < 18.5) {
+		var interp = "Underweight"
+	} else {
+		if (bmi < 25.0) {
+			var interp = "Normal"
+		} else {
+			if (bmi < 30.0) {
+				var interp = "Overweight"
+			} else {
+				var interp = "Obese"
+			}
+		}
+	}
+
+        form.bsa.value = bsa;
+        form.leanKg.value = leanKg;
+	form.leanLbs.value = leanLbs;
+        form.idealKg.value = idealKg;
+	form.idealLbs.value = idealLbs;
+        form.bmi.value = bmi;
+	form.interp.value = interp;
+
+	return true
+}
+
+
+function checkNum(val,text) {
+        if ((val == null) || (isNaN(val)) || (val == "") || (val < 0)) {
+		alert("Please enter a value for " + text + ".");
+                return false
+        }
+        return true;
+}
+
+
+function rounding(number,decimal) {
+	multiplier = Math.pow(10,decimal);
+	number = Math.round(number * multiplier) / multiplier;
+        return number
+}
+</script>
+
+<br>
+<div class="ancalc">
+
+<table border="0" cellpadding="2" cellspacing="0">
+      <tbody>
+<tr>
+          <th class="top" colspan="2" style="background-color: #ff6600;">Vücut Kitle İndeksi Hesaplama</th>
+        </tr>
+<tr>
+          <th colspan="2"><span style="font-family: Arial, Helvetica, sans-serif; font-size: x-small;">Bilgilerinizi Giriniz </span></th>
+        </tr>
+<tr>
+          <td class="left"><span style="font-family: Arial, Helvetica, sans-serif;">Cinsiyet</span></td>
+          <td><span style="font-family: Arial, Helvetica, sans-serif;"><input checked="" name="sex" type="radio">
+Erkek 
+            <input name="sex" type="radio">
+            Bayan </span></td>
+        </tr>
+<tr>
+          <td><span style="font-family: Arial, Helvetica, sans-serif;">Kilonuz</span></td>
+          <td><span style="font-family: Arial, Helvetica, sans-serif;"><input maxlength="4" name="wt" size="3" type="text" value=""> 
+                    <select name="wu">
+                      <option>lbs 
+                      </option><option>kg </option></select> <span class="style2" style="font-size: xx-small;">Lütfen Seçin<a href="http://kadinlarve.blogspot.com/">.</a></span></span></td>
+        </tr>
+<tr>
+          <td><span style="font-family: Arial, Helvetica, sans-serif;">Boyunuz</span></td>
+          <td><span style="font-family: Arial, Helvetica, sans-serif;"><input maxlength="3" name="ht" size="3" type="text" value=""> 
+                    <select name="hu">
+                      <option>inches 
+                      </option><option>cm 
+                      </option><option>meters </option></select><span class="style2" style="font-size: xx-small;">Lütfen Seçin<a href="http://kadinlarve.blogspot.com/">.</a></span></span></td>
+        </tr>
+<tr>
+          <td colspan="2"><span style="font-family: Arial, Helvetica, sans-serif;"><input name="button" onclick="bmiCalc(this.form)" type="button" value="Hesapla"></span></td>
+        </tr>
+<tr>
+          <th colspan="2"><span style="font-family: Arial, Helvetica, sans-serif;">Sonuçlar </span></th>
+        </tr>
+<tr>
+          <td><span style="font-family: Arial, Helvetica, sans-serif;">Vücut Yüzey Alanı </span></td>
+          <td><span style="font-family: Arial, Helvetica, sans-serif;"><input class="output" maxlength="5" name="bsa" size="5" type="text" value="">
+m<span style="color: #444444;"><a href="http://kadinlarve.blogspot.com/"><sup>2</sup></a></span></span></td>
+        </tr>
+<tr>
+          <td><span style="font-family: Arial, Helvetica, sans-serif;"> Yalın Vücut 
+   Ağırlığı</span></td>
+          <td><span style="font-family: Arial, Helvetica, sans-serif;"> 
+                    <input class="output" maxlength="5" name="leanKg" size="5" type="text" value="">
+                    kg = 
+                    <input class="output" maxlength="5" name="leanLbs" size="5" type="text" value="">
+                    lbs </span></td>
+        </tr>
+<tr>
+          <td><span style="font-family: Arial, Helvetica, sans-serif;"> İdeal Vücut Ağırlığı</span></td>
+          <td><span style="font-family: Arial, Helvetica, sans-serif;"> 
+                    <input class="output" maxlength="5" name="idealKg" size="5" type="text" value="">
+                    kg = 
+                    <input class="output" maxlength="5" name="idealLbs" size="5" type="text" value="">
+                    lbs </span></td>
+        </tr>
+<tr>
+          <td><span style="font-family: Arial, Helvetica, sans-serif;"> Vücut Kitle İndeksi</span></td>
+          <td><span style="font-family: Arial, Helvetica, sans-serif;"> 
+                    <input class="output" maxlength="5" name="bmi" size="5" type="text" value="">
+                    kg/m<span style="color: black;"><a href="http://kadinlarve.blogspot.com/"><sup>2</sup></a></span> = 
+                    <input class="output" maxlength="12" name="interp" size="12" type="text" value="">
+                    </span></td>
+        </tr>
+<tr>
+          <th colspan="2"><span style="font-family: Arial, Helvetica, sans-serif;">Sonuçların Analizi</span></th>
+        </tr>
+<tr>
+          <td><span style="font-family: Arial, Helvetica, sans-serif;"> 
+          18.5'den Küçükse</span></td>
+          <td><span style="font-family: Arial, Helvetica, sans-serif;">Zayıf</span></td>
+        </tr>
+<tr>
+          <td><span style="font-family: Arial, Helvetica, sans-serif;"> 
+          18.5 ile 24.9</span></td>
+          <td><span style="font-family: Arial, Helvetica, sans-serif;">Sağlıklı</span></td>
+        </tr>
+<tr>
+          <td style="height: 30px;"><span style="font-family: Arial, Helvetica, sans-serif;">25 ile 29.9 Arası</span></td>
+          <td style="height: 30px;"><span style="font-family: Arial, Helvetica, sans-serif;">Kilolu</span></td>
+        </tr>
+<tr>
+          <td><span style="font-family: Arial, Helvetica, sans-serif;"> 
+          30 ve Yukarısı </span></td>
+          <td><span style="font-family: Arial, Helvetica, sans-serif;">Obez</span></td>
+        </tr>
+<tr>
+          <td colspan="2"><center>
+<a href="http://kadinlarve.blogspot.com/"><span style="font-family: Arial, Helvetica, sans-serif; font-size: xx-small;"> 
+   <span class="style1">Kadın Ve Yaşam</span></span></a></center>
+</td>
+        </tr>
+</tbody>
+    </table>
+<script language="JavaScript" type="text/javascript">recalc_onclick('');  </script>  
+    <noscript>
+    The browser does not support JavaScript. The calculations will not work.
+    </noscript>
+  
+</div>
+                    
+                    	
+    
+    
+
+						
+					</div>
+				</div>
+			</div>
